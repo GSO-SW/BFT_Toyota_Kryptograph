2b6a2941f0ba8138cbbf743e002537aa37258e7e
bjar.rashid@gmail.com <bjar.rashid@gmail.com>
2022-05-13 09:04:43 +0200

    Update

--- KryptographBibliothek/ZeichenEntfernen.cs
+++ KryptographBibliothek/ZeichenEntfernen.cs
@@ -9,12 +9,11 @@ namespace KryptographBibliothek
 
 
     {
-        public static void Zeichen_Entfernen()
+        public static string Zeichen_Entfernen(string str)
 
 
 
         {
-            string str = "_-A a k Kly ulbl KbhsZluzl Jvuayvssly pza pu Mburapvuzbtmhun buk Clyhyilpabun lpul rshyl Clyilzzlybun gbt KbhsZovjr 4, hsslykpunz mpls tpy kpl nlypunl Hrrb-Shbmglpa däoyluk klz Alzaz ulnhapc hbm, kpl cvy hsslt Jvyl Nhtly zaöylu küymal. Lilumhssz upjoa glpanltäß pza kpl Nyößl kly pualyulu Mlzawshaal, dlsjol upjoa 1 AI, zvuklyu uby 825 NI nyvß pza, cvu klulu dplklybt uby 667 NI nlubaga dlyklu röuulu, kh kly Ylza cvt Zfzalt ilslna dpyk. Pu Glpalu, pu klulu Zwplsl hbjo zjovuths ipz gb 100 NI nyvß zpuk, dlyklu oply zjoulss Wshagwyvisltl luazalolu. Kpl Rvuzvsl bualyzaüaga lydlpalyal Zwlpjolytönspjorlpalu pu Mvyt cvu BZI-Shbmdlyrlu, hsslykpunz röuulu oply uby WZ4-Zwplsl hbznlshnlya dlyklu. Kly pualyul Zwlpjoly rhuu müy WZ5-Zwplsl tpa ZZK-Lydlpalybunzrhyalu (khur T.2.-Zjoupaazalssl) clynyößlya dlyklu, hsslykpunz dpyk kplzlz Mlahbyl ilp Ylslhzl uvjo upjoa gby Clymünbunz zalolu, kluu lz pza lyza müy lpu gbrüumapnlz Bwkhal nlwshua. Lz islpia higbdhyalu, dpl zpjo kpl WZ5 pt Shunglpaalza zjoshnlu dpyk. Khur Hidäyazrvtwhapipspaäa, klu Shbujo-Apalsu buk klu Lersbzpc-Zwplslu khym hily qlaga zjovu nlyul tpa Zvufz Ulea-Nlu-Rvuzvsl nlspliäbnlsa dlyklu. ";
 
 
 
@@ -35,7 +34,7 @@ namespace KryptographBibliothek
 
 
 
-                Console.WriteLine("Entfernen Sie Ihr zeichen?(Um den Programm zu beenden schreiben Sie ´exit`");
+                Console.WriteLine("Entfernen Sie Ihr zeichen?");
                 Console.WriteLine("Um den Programm zu beenden schreiben Sie ´exit`!");
                 Console.Write("Eingabe:");
                 
@@ -84,16 +83,15 @@ namespace KryptographBibliothek
                 Console.Clear();
 
 
-
+                Console.WriteLine("Möchten Sie nochmal wiederholen");
+               
             } while (true);
 
 
 
-
-
-
-
+            return str;
         }
+       
     }
 }
     
