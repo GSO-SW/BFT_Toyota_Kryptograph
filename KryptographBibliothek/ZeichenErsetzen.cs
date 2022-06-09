e51215045438cf7f46b28cd416b7dd9a2698c9de
Hamza Modric <hamza.m9@gso.schule.koeln>
2022-06-09 11:13:49 +0200

    programm fertig

--- KryptographBibliothek/ZeichenErsetzen.cs
+++ KryptographBibliothek/ZeichenErsetzen.cs
@@ -2,85 +2,50 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace KryptographBibliothek
 {
     public class ZeichenErsetzen
     {   
-        public static void Ersetzen()
+        public static string Ersetzen(string chiffre, Dictionary<String,double> tabelle_chiffre, Dictionary<String,double>tabelle_sprache)
         {
 
-
-            string chiffre = "Kly ulbl";
-
-            Dictionary<String, double> tabelle_chiffre = new Dictionary<string, double>();
-
-            tabelle_chiffre.Add("K", 1/7.0);
-            tabelle_chiffre.Add("L", 3 / 7.0);
-            tabelle_chiffre.Add("Y", 1 / 7.0);
-            tabelle_chiffre.Add("U", 1 / 7.0);
-            tabelle_chiffre.Add("B", 1 / 7.0);
+            { 
 
 
 
+                chiffre = chiffre.ToUpper();
+                var tabelle_sprache_sorted = from entry in tabelle_sprache orderby entry.Value descending select entry;
+                var tabelle_chiffre_sorted = from entry in tabelle_chiffre orderby entry.Value ascending select entry;
 
-            Dictionary<String, double> Tabelle = new Dictionary<string, double>();
-            {
+                tabelle_sprache = tabelle_sprache.OrderByDescending(u => u.Value).ToDictionary(z => z.Key, y => y.Value);
+                tabelle_chiffre = tabelle_chiffre.OrderByDescending(u => u.Value).ToDictionary(z => z.Key, y => y.Value);
 
-                Tabelle.Add("A", 0.0558);
-                Tabelle.Add("Ä", 0.0054);
-                Tabelle.Add("B", 0.0196);
-                Tabelle.Add("C", 0.0316);
-                Tabelle.Add("D", 0.0498);
-                Tabelle.Add("E", 0.1693);
-                Tabelle.Add("F", 0.0149);
-                Tabelle.Add("G", 0.0302);
-                Tabelle.Add("H", 0.0498);
-                Tabelle.Add("I", 0.0802);
-                Tabelle.Add("J", 0.0024);
-                Tabelle.Add("K", 0.0132);
-                Tabelle.Add("L", 0.0360);
-                Tabelle.Add("M", 0.0255);
-                Tabelle.Add("N", 0.1053);
-                Tabelle.Add("O", 0.0224);
-                Tabelle.Add("Ö", 0.0030);
-                Tabelle.Add("P", 0.0067);
-                Tabelle.Add("Q", 0.0002);
-                Tabelle.Add("R", 0.0689);
-                Tabelle.Add("ß", 0.0037);
-                Tabelle.Add("S", 0.0642);
-                Tabelle.Add("T", 0.0579);
-                Tabelle.Add("U", 0.0383);
-                Tabelle.Add("Ü", 0.0065);
-                Tabelle.Add("V", 0.0084);
-                Tabelle.Add("W", 0.0178);
-                Tabelle.Add("X", 0.0005);
-                Tabelle.Add("Y", 0.0005);
-                Tabelle.Add("Z", 0.0121);
+                StringBuilder ch = new StringBuilder(chiffre);
 
-      
-                var Tabelle_sorted = from entry in Tabelle orderby entry.Value ascending select entry;
-                foreach (KeyValuePair<string, double> kvp in Tabelle_sorted)
+
+                int index = 0;
+                for(int i = 0; i < chiffre.Length; i++)
                 {
-                    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-                }
-                Console.WriteLine("______________________________");
-                var Chiffre_sorted = from entry in tabelle_chiffre orderby entry.Value ascending select entry;
-                foreach (KeyValuePair<string, double> kvp in Tabelle_sorted)
-                {
-                    Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-                }
-                for (int i = 0; i < Chiffre_sorted.Count(); i++)
-                {
-                    string key_chiffre = Chiffre_sorted.ElementAt(tabelle_chiffre.Count - 1 - i).Key;
-                    chiffre = chiffre.Replace(' ', ',');
+                    for (int r = 0; r < tabelle_chiffre.Count; r++)
+                    {
+                        if (tabelle_chiffre.Keys.ElementAt(r) == chiffre[i].ToString())
+
+
+                            index = r;
+
+                        
+
+                    }
+
+                    ch[i] = tabelle_sprache.Keys.ElementAt(index).ToCharArray()[0];
+                    
                 }
 
 
-
-
-
-
+                Console.WriteLine(ch.ToString());
+                return ch.ToString();
             }
         }
     }
